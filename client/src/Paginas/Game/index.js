41e45ba701d350b55b802934511903635e4cedbc
guga-tab <gustavomachado.521@gmail.com>
2025-05-29 16:56:14 -0300

    Estruturando o projeto em pastas

---
+++ client/src/Paginas/Game/index.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+export default function Game(){
+    const [games, setGames] = useState([])
+
+    useEffect(
+        function(){
+            
+            async function consultar(params) {
+                
+            }
+        }
+    )
+}
---
+++ client/src/Paginas/Sobre/index.js
@@ -0,0 +1,8 @@
+function Sobre(){
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Sobre
